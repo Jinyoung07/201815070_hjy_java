0c0289f41b8c945813f418f8e678219556ecd7a8
jinyoung07 <jinyovng07@gmail.com>
2021-11-09 23:35:05 +0900

    Add Java File [Homework5.java]

---
+++ Homework5.java
@@ -0,0 +1,41 @@
+package Homework;
+
+public class PersonalComputer {
+    private String CPU;
+    private double memory;
+
+    public PersonalComputer(String companyName, double memory) {
+        this.CPU = CPU;
+        this.memory = memory;
+    }
+
+    public void show() {
+        System.out.println("CPU: " + CPU + ", memory: " + memory);
+    }
+
+    public static class Monitor {
+        private String color;
+        private double monitorSize;
+        private String power;
+
+        public Monitor(String color, double monitorSize, String power) {
+
+            this.color = color;
+            this.monitorSize = monitorSize;
+            this.power = power;
+        }
+    }
+
+    public static class Computer {
+        private String color;
+        private String power;
+
+        public Computer(String CPU, double price, String color, String power) {
+            this.color = color;
+            this.power = power;
+        }
+        public void show() {
+            System.out.println("Color: " + color + ", Power: " + power);
+        }
+    }
+}
