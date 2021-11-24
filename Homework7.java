64ac3acdf9df67959653ddf28e62367e9737c3c7
jinyoung07 <jinyovng07@gmail.com>
2021-11-24 14:42:36 +0900

    Add Java File [Homework7.java]

---
+++ Homework7.java
@@ -0,0 +1,82 @@
+package Homework;
+
+public class Homework7 {
+    public static void main(String[] args) {
+        InkjetPrinter Inkjet = new InkjetPrinter("Brother DCP-T720DW", 7500);
+        LaserPrinter laser = new LaserPrinter("Canon MFC643Cdw", 1500);
+        //run the print method until it returns true
+        while (Inkjet.print());
+        while (laser.print());
+    }
+
+    public abstract static class Printer {
+        private String model;
+        private int printedCount;
+        private int availableCount;
+
+        public Printer(String model, int availableCount) {
+            this.model = model;
+            this.availableCount = availableCount;
+            this.printedCount = 0;
+        }
+
+        public String getModel() {
+            return this.model;
+        }
+
+        public int getPrintedCount() {
+            return this.printedCount;
+        }
+
+        public int getAvailableCount() {
+            return this.availableCount;
+        }
+
+        public void incrementPrintedCount() {
+            printedCount++;
+        }
+
+        public void decrementAvailableCount() {
+            availableCount--;
+        }
+        public abstract boolean print();
+    }
+
+    public static class InkjetPrinter extends Printer {
+        public InkjetPrinter(String model, int printedCount) {
+            super(model, printedCount);
+        }
+        public boolean print() {
+            if(getAvailableCount() >= 1)
+            {
+                incrementPrintedCount();
+                decrementAvailableCount();
+                return true;
+            }
+            else
+            {
+                System.out.println(getModel() + ": " + (getPrintedCount() + 1) + "th Print Failed - Out of Ink.");
+                return false;
+            }
+        }
+    }
+
+    public static class LaserPrinter extends Printer {
+        public LaserPrinter(String model, int printedCount) {
+            super(model, printedCount);
+        }
+        public boolean print() {
+            if(getAvailableCount() >= 1)
+            {
+                incrementPrintedCount();
+                decrementAvailableCount();
+                return true;
+            }
+            else
+            {
+                System.out.println(getModel() + ": " + (getPrintedCount() + 1) + "th Print Failed - Low Toner.");
+                return false;
+            }
+        }
+    }
+}
